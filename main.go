28d00b4dc377bfbb5eab1225110c638ca45ab57f
Alexander Schetilov <aschetilov@gmail.com>
2021-06-25 23:14:07 +0300

    ya durak

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"regexp"
 )
 
 func main() {
-	input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Print(WordCount(input))
-}
-
-func WordCount(value string) int {
-	results := regexp.MustCompile(`[\S]+`).FindAllString(value, -1)
-	return len(results)
+	input := os.Args[1:]
+	fmt.Print(len(input))
 }
